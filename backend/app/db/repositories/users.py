ba0d7f21dc1d19c2a4d3547799d1c16d98bae6d1
Hadi Mini <hadimini@gmail.com>
2025-08-07 15:02:46 +0300

    login

--- backend/app/db/repositories/users.py
+++ backend/app/db/repositories/users.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from databases import Database
 from fastapi import HTTPException, status
 from pydantic import EmailStr
@@ -67,3 +69,14 @@ class UsersRepository(BaseRepository):
         created_user = await self.db.fetch_one(query=REGISTER_NEW_USER_QUERY, values=new_user_params.dict())
 
         return UserInDB(**created_user)
+
+    async def authenticate_user(self, *, email: EmailStr, password: str) -> Optional[UserInDB]:
+        user = await self.get_user_by_email(email=email)
+
+        if not user:
+            return None
+
+        if not self.auth_service.verify_password(password=password, salt=user.salt, hashed_pw=user.password):
+            return None
+
+        return user
