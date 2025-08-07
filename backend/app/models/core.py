0a5a25fd44937bbfd4534184bfa886d32108aeaa
Hadi Mini <hadimini@gmail.com>
2025-08-07 11:35:07 +0300

    users

--- backend/app/models/core.py
+++ backend/app/models/core.py
@@ -1,4 +1,7 @@
-from pydantic import BaseModel
+from datetime import datetime
+from typing import Optional
+
+from pydantic import BaseModel, validator
 
 
 class CoreModel(BaseModel):
@@ -10,3 +13,12 @@ class CoreModel(BaseModel):
 
 class IDModelMixin(BaseModel):
     id: int
+
+
+class DateTimeModelMixin(BaseModel):
+    created_at: datetime
+    updated_at: datetime
+
+    @validator('created_at', 'updated_at', pre=True)
+    def default_datetime(cls, value: datetime) -> datetime:
+        return value or datetime.now()
