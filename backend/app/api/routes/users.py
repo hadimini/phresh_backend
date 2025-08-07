e164a2aff0f3036916d63ebb7513a219774ed5a8
Hadi Mini <hadimini@gmail.com>
2025-08-07 13:58:47 +0300

    add_access_token_on_registration

--- backend/app/api/routes/users.py
+++ backend/app/api/routes/users.py
@@ -2,9 +2,12 @@ from fastapi import Depends, APIRouter, HTTPException, Path, Body
 from starlette.status import HTTP_201_CREATED, HTTP_404_NOT_FOUND
 
 from app.api.dependencies.database import get_repository
+from app.db.repositories.users import UsersRepository
+from app.models.token import AccessToken
 from app.models.user import UserCreate, UserPublic
 
-from app.db.repositories.users import UsersRepository
+from app.services import auth_service
+
 
 router = APIRouter()
 
@@ -15,6 +18,8 @@ async def register_new_user(
         user_repo: UsersRepository = Depends(get_repository(UsersRepository)),
 ) -> UserPublic:
     created_user = await user_repo.register_new_user(new_user=new_user)
+    access_token = AccessToken(
+        access_token=auth_service.create_access_token_for_user(user=created_user), token_type="bearer"
+    )
 
     return created_user
-
