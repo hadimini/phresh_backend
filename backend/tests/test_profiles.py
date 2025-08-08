c37fe33d1ea7ece5951ff4fb045a0748970b070d
Hadi Mini <hadimini@gmail.com>
2025-08-08 08:56:39 +0300

    profiles_repo

--- backend/tests/test_profiles.py
+++ backend/tests/test_profiles.py
@@ -4,11 +4,12 @@ from databases import Database
 from fastapi import FastAPI, status
 from httpx import AsyncClient
 
-from app.models.user import UserInDB
+from app.models.user import UserInDB, UserPublic
+from app.models.profile import ProfileInDB, ProfilePublic
+from app.db.repositories.profiles import ProfilesRepository
 
 
 pytestmark = pytest.mark.asyncio
-
 
 
 class TestProfilesRoutes:
@@ -29,3 +30,17 @@ class TestProfilesRoutes:
             json={'profile_update': {}}
         )
         assert res.status_code != status.HTTP_404_NOT_FOUND
+
+
+class TestProfileCreate:
+    async def test_profile_created_for_new_users(self, app: FastAPI, client: AsyncClient, db: Database) -> None:
+        profiles_repo = ProfilesRepository(db)
+
+        new_user = {"email": "dwayne@johnson.io", "username": "therock", "password": "dwaynetherockjohnson"}
+        res = await client.post(app.url_path_for("users:register-new-user"), json={"new_user": new_user})
+        assert res.status_code == status.HTTP_201_CREATED
+
+        created_user = UserPublic(**res.json())
+        user_profile = await profiles_repo.get_profile_by_user_id(user_id=created_user.id)
+        assert user_profile is not None
+        assert isinstance(user_profile, ProfileInDB)
