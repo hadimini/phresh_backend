80680d013d147c221349b93f14094e915fee166e
Hadi Mini <hadimini@gmail.com>
2025-08-08 08:59:02 +0300

    create_profile_on_user_registration

--- backend/app/db/repositories/users.py
+++ backend/app/db/repositories/users.py
@@ -5,6 +5,8 @@ from fastapi import HTTPException, status
 from pydantic import EmailStr
 
 from app.db.repositories.base import BaseRepository
+from app.db.repositories.profiles import ProfilesRepository
+from app.models.profile import ProfileCreate
 from app.models.user import UserCreate, UserUpdate, UserInDB, UserPublic
 from app.services import auth_service
 
@@ -32,6 +34,7 @@ class UsersRepository(BaseRepository):
     def __init__(self, db: Database) -> None:
         super().__init__(db)
         self.auth_service = auth_service
+        self.profiles_repo = ProfilesRepository(db)
 
     async def get_user_by_email(self, *, email: EmailStr) -> UserInDB:
         user_record = await self.db.fetch_one(query=GET_USER_BY_EMAIL_QUERY, values={"email": email})
@@ -67,7 +70,9 @@ class UsersRepository(BaseRepository):
         user_password_update = self.auth_service.create_salt_and_hashed_password(plaintext_password=new_user.password)
         new_user_params = new_user.copy(update=user_password_update.dict())
         created_user = await self.db.fetch_one(query=REGISTER_NEW_USER_QUERY, values=new_user_params.dict())
-
+        await self.profiles_repo.create_profile_for_user(
+            profile_create=ProfileCreate(user_id=created_user['id'])
+        )
         return UserInDB(**created_user)
 
     async def authenticate_user(self, *, email: EmailStr, password: str) -> Optional[UserInDB]:
